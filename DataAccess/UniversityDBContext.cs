571eafee4ab4509ee9a13b8e34c36a451c2e2cae
MichaelNossa <michaelnossab@gmail.com>
2022-11-03 08:34:24 -0500

    Clase Curso

---
+++ DataAccess/UniversityDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Backend.Models.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.DataAccess
+{
+    public class UniversityDBContext :  DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options)
+        {
+
+        }
+
+        // TODO : Add DbSets (Tables of our Data Base)
+        public DbSet<User>? Users { get; set; } 
+    }
+}
---
+++ Models/DataModels/Curso.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.DataModels
+{
+    public class Curso : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string DescriptionSmall { get; set; } = string.Empty;
+
+        [Required]
+        public string DescriptionLarge { get; set; } = string.Empty;
+
+        [Required]
+        public string PublicObject { get; set; } = string.Empty;
+
+        [Required]
+        public string Objects { get; set; } = string.Empty;
+
+        [Required]
+        public string Requirements { get; set; } = string.Empty;
+
+        [Required]
+        public Nivel Nivel { get; set; }
+    }
+    public enum Nivel
+    {
+        Básico,
+        Intermedio,
+        Avanazado
+    }
+
+}
